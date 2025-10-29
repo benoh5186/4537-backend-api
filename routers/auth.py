26cd7c5ef8c94cbf98371134062d8ecf7bc17dc7
Ben Oh <moh16@my.bcit.ca>
2025-10-29 15:04:25 -0700

    add .env

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,15 @@
 from fastapi import APIRouter, HTTPException, Form, Request, Response
+import jwt
 
 
 router = APIRouter()
 
+
+@router.post("v1/login/")
+def handle_login():
+    pass
+
+
+@router.post("v1/signup")
+def handle_signup():
+    pass 
