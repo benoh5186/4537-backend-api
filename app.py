1b5aa3e2d8d914a2e9c4b7b0e101129f9150a38e
Ben Oh <moh16@my.bcit.ca>
2025-10-29 15:01:52 -0700

    add app.py and auth.py for endpoint routing

---
+++ app.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+app = FastAPI()
+
---
+++ routers/auth.py
@@ -0,0 +1,5 @@
+from fastapi import APIRouter, HTTPException, Form, Request, Response
+
+
+router = APIRouter()
+
