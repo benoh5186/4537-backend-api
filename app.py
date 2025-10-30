c50587876d53e15a797d57364e2259c4c477f483
Ben Oh <moh16@my.bcit.ca>
2025-10-30 15:10:37 -0700

    add middleware and include routers to FastAPI

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
+from crypt import methods
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers import auth
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = FastAPI()
+
+middleware = CORSMiddleware(
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"]
+)
+
+routers = [
+    auth.router 
+]
+
+for router in routers:
+    app.include_router(router)
 
