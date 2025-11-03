6e09e492ad77cf904c7964a5b716991fdc527292
MarcusLages <marcusvlages@gmail.com>
2025-11-03 10:52:28 -0800

    trying out small fix for cors in backend

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class App:
         self.__app.add_middleware(
                 CORSMiddleware,
                 allow_origins=["*"],
-                # allow_credentials=True,
+                allow_credentials=True,
                 allow_methods=["*"],
                 allow_headers=["*"]
             )
