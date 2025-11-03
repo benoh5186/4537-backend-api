1db863a3e0ee23a8adf749d3ff799b129659bc8e
Ben Oh <moh16@my.bcit.ca>
2025-11-02 17:11:16 -0800

    test

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class App:
         """
         self.__app.add_middleware(
                 CORSMiddleware,
-                allow_origins=["https://4537-project-frontend.netlify.app"],
+                allow_origins=["*"],
                 allow_credentials=True,
                 allow_methods=["*"],
                 allow_headers=["*"]
