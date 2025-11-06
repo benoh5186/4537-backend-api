9f863ce1f68dc1261fd69e600748e62c5e9b05e0
Ben Oh <moh16@my.bcit.ca>
2025-11-06 10:30:17 -0800

    add authentication to ai endpoint

--- routers/ai.py
+++ routers/ai.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, HTTPException, Request 
+from fastapi import APIRouter, HTTPException, Request, status
+from auth import AuthUtility
 import httpx
 
 
@@ -7,22 +8,26 @@ router = APIRouter()
 
 @router.post("/api/service/ai")
 async def handle_ai_json(request: Request):
-    body = await request.json()
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            "https://four537-ai-backend.onrender.com/v1/json/parse",
-            json={"text" : body["text"], "lang" : body["lang"]}
-        )
-        if response.is_success:
-            data = response.json()
-            return {"data" : data["data"]}
-        else:
-            raise HTTPException(
-                # 400
-                status_code=response.status_code,
-                detail={
-                    "message" : "nah"
-                }
+    is_auth = AuthUtility.authenticate(request)
+    if is_auth:
+        body = await request.json()
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                "https://four537-ai-backend.onrender.com/v1/json/parse",
+                json={"text" : body["text"], "lang" : body["lang"]}
             )
+            if response.is_success:
+                data = response.json()
+                return {"data" : data["data"]}
+            else:
+                raise HTTPException(
+                    # 400
+                    status_code=response.status_code,
+                    detail={
+                        "message" : "nah"
+                    }
+                )
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
             
 
