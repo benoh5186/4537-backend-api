457eda2223759444c99d2ef6f65f66d7157f33af
Ben Oh <moh16@my.bcit.ca>
2025-11-23 15:21:36 -0800

    add ai schema to json endpoint

--- routers/ai.py
+++ routers/ai.py
@@ -4,6 +4,9 @@ import httpx
 
 class AI:
     __AI_TEXT_TO_JSON_ENDPOINT = "/api/v1/service/ai/text"
+    __AI_SCHEMA_TO_JSON_ENDPOINT = "/api/v1/service/ai/schema"
+
+
     def __init__(self, db):
         self.__router = APIRouter()
         self.__db = db
@@ -42,5 +45,34 @@ class AI:
                     )
         else:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    async def __handle_ai_schema_json(self, request: Request):
+        endpoint_info = {"method" : "POST", "endpoint" : self.__AI_SCHEMA_TO_JSON_ENDPOINT}
+        self.__db.update_endpoint(endpoint_info)
+        payload = AuthUtility.authenticate(request)
+        AuthUtility.increase_api_usage(payload, self.__db)
+        api_usage = AuthUtility.get_api_usage(payload, self.__db)
+        if payload:
+            body = await request.json()
+            async with httpx.AsyncClient() as client:
+                response = await client.post(
+                    "https://4537-ai-backend-production.up.railway.app/v1/json/schemedParse",
+                    json={"text" : body["text"], "lang" : body["lang"], "schema" : body["schema"]}
+                )
+                if response.is_success:
+                    data = response.json()
+                    return {"data" : data["data"], "api_usage" : api_usage}
+                else:
+                    print("unsuccessful")
+                    raise HTTPException(
+                        # 400
+                        status_code=response.status_code,
+                        detail={
+                            "message" : "nah"
+                        }
+                    )
+        else:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
             
 
