e6197c69927e32f41e6a86683440609521d1a4dd
Ben Oh <moh16@my.bcit.ca>
2025-11-23 06:31:08 -0800

    add helper methods to profile router class

--- routers/profile.py
+++ routers/profile.py
@@ -29,6 +29,8 @@ class ProfileRouter:
                 uid = int(payload["sub"])
                 user_data = await request.json()
                 password_schema = Password(**user_data)
+                if self.__check_password_equality(request, password_schema.password):
+                    raise HTTPException(status_code=status.HTTP_409_CONFLICT)
                 hashed_password = bcrypt.hashpw(password_schema.password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
                 self.__db.change_password(uid, hashed_password)
                 return {"message" : "password change success"}
@@ -46,15 +48,35 @@ class ProfileRouter:
                 uid = int(payload["sub"])
                 user_data = await request.json()
                 email_schema = Email(**user_data)
+                if self.__check_email_equality(request, email_schema.email):
+                    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail={"same_email" : True})
                 is_changed = self.__db.change_email(uid, email_schema.email)
                 if is_changed:
                     return {"message" : "email change success", "new_email" : email_schema.email}
                 else:
-                    raise HTTPException(status_code=status.HTTP_409_CONFLICT)
+                    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail={"same_email" : False})
             else:
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
         except ValidationError:
             raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    def __check_email_equality(self, request: Request, new_email):
+        payload = AuthUtility.get_jwt_payload(request)
+        uid = int(payload["sub"])
+        user_info = self.__db.find_user(uid)
+        if new_email == user_info["email"]:
+            return True
+        return False 
+
+    def __check_password_equality(self, request: Request, new_password):
+        payload = AuthUtility.get_jwt_payload(request)
+        uid = int(payload["sub"])
+        user_info = self.__db.find_user(uid)
+        new_password_bytes = new_password.encode('utf-8')
+        old_password_bytes = user_info["password"].encode('utf-8')
+        if bcrypt.checkpw(new_password_bytes, old_password_bytes):
+            return True
+        return False
 
 
 
